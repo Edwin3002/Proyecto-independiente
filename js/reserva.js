760c152a738e484bf9fa19050f399c1faf301728
Sebastián <arevalosebastian081@gmail.com>
2022-02-15 23:50:09 -0500

    parte_sebastian_reservasYnosotros

--- js/reserva.js
+++ js/reserva.js
@@ -1,40 +1,23 @@
-apifood = [
-    {
-        name: 'carne',
-        img: './img/1.jpg',
-        description: 'This is a wider card with supporting text below as a natural lead-in tocontent.',
-        price: '20000',
-        categorie: 'fuertes'
-    },
-    {
-        name: 'carne',
-        img: './img/1.jpg',
-        description: 'This is a wider card with supporting text below as a natural lead-in tocontent.',
-        price: '20000',
-        categorie: 'fuertes'
-    },
-    {
-        name: 'carne',
-        img: './img/1.jpg',
-        description: 'This is a wider card with supporting text below as a natural lead-in tocontent.',
-        price: '20000',
-        categorie: 'fuertes'
-    },
-    {
-        name: 'carne',
-        img: './img/1.jpg',
-        description: 'This is a wider card with supporting text below as a natural lead-in tocontent.',
-        price: '20000',
-        categorie: 'fuertes'
-    },
-]
-let cards = apifood;
+const API_all = async () => {
+    const data = await fetch('https://proyecto-independiente.herokuapp.com/menu');
+    const json = await data.json()
+    return json
+}
 
-function pintarCartas(cartas){
+const API_categoria = async (categoria) => {
+    const data = await fetch(`https://proyecto-independiente.herokuapp.com/menu?categoria=${categoria}`);
+    const json = await data.json()
+    return json
+}
+
+document.addEventListener('DOMContentLoaded', pintarCartas__all())
+
+async function pintarCartas__all(){
     let space = document.getElementById('cartas')
+    let data = await API_all()
     space.innerHTML = ''
-    cartas.forEach(card => {
-        const {name, img, description, price, categorie} = card;
+    data.forEach(element => {
+        const {nombre, descripcion, img, precio, categoria} = element;
         space.innerHTML += `
         <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
@@ -43,13 +26,40 @@ function pintarCartas(cartas){
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title text-white text-uppercase">${name}</h5>
-                    <p class="card-text ">${description}</p>
-                    <p class="card-text fw-bold">${price}$</p>
+                    <h5 class="card-title text-white text-uppercase">${nombre}</h5>
+                    <p class="card-text ">${descripcion}</p>
+                    <p class="card-text fw-bold">${precio}$</p>
                 </div>
             </div>
         </div>
     </div>`
     });
 }
-pintarCartas(cards)
+
+async function pintarCartas__categoria(categoria){
+    let space = document.getElementById('cartas')
+    let data = await API_categoria(categoria)
+    space.innerHTML = ''
+    data.forEach(element => {
+        const {nombre, descripcion, img, precio, categoria} = element;
+        space.innerHTML += `
+        <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${img}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title text-white text-uppercase">${nombre}</h5>
+                    <p class="card-text ">${descripcion}</p>
+                    <p class="card-text fw-bold">${precio}$</p>
+                </div>
+            </div>
+        </div>
+    </div>`
+    });
+}
+
+
+
+
