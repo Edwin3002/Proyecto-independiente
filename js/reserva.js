c6c8d8fcec5d87e4c7215bb1aa20431c1210f1fa
Edwin <vargas11742@gmail.com>
2022-02-17 16:44:46 -0500

    v4 puliendo cositas de la tabla y formulario

--- js/reserva.js
+++ js/reserva.js
@@ -13,6 +13,7 @@ const API_categoria = async (categoria) => {
 
 document.addEventListener('DOMContentLoaded', pintarCartas__all())
 
+// pinta todo
 async function pintarCartas__all() {
     let space = document.getElementById('cartas')
     let data = await API_all()
@@ -22,7 +23,7 @@ async function pintarCartas__all() {
         space.innerHTML += `
         <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
-            <div class="col-md-4">
+            <div class="col-md-4 d-flex align-items-center">
                 <img src="${img}" class="img-fluid rounded-start" alt="...">
             </div>
             <div class="col-md-8">
@@ -37,6 +38,7 @@ async function pintarCartas__all() {
     });
 }
 
+// pinta por el filtro categorias
 async function pintarCartas__categoria(categoria) {
     let space = document.getElementById('cartas')
     let data = await API_categoria(categoria)
@@ -61,15 +63,47 @@ async function pintarCartas__categoria(categoria) {
     });
 }
 
+
+// pintar elementos en el carrito
+let pedidoCarrito = []
+let x = 0
 function guardarCarrito(img, name) {
-    console.log(img, name)
     let carrito = document.getElementById('listarCarrito');
+
+    if (true) {
+        x++;
+        pedidoCarrito += [
+            name + ', '
+
+        ];
+        console.log(pedidoCarrito)
+        console.log(pedidoCarrito.length)
+        console.log(x)
+    }
+    let total = document.getElementById('listasNumeroP');
+    total.innerHTML = `<tr>
+            <td>Total de pedidos</td>
+            <td>${x}</td>
+                    </tr>`
     carrito.innerHTML += `
     <tr>
                     <td><div class="divCarrito"><img src="${img}" alt=""></div></td>
                     <td>${name}</td>
                 </tr>
     `
+
 }
+
+function confirmarP() {
+    let nombre = document.getElementById("inputNombre").value;
+    let telefono = document.getElementById("inputTelefono").value;
+    let mesa = document.getElementById("inputMesa").value;
+    let hora = document.getElementById("inputHora").value;
+
+    let pedido = [nombre, telefono, mesa, hora];
+    console.log(pedido)
+    alert(`Se han confirmado ${x} pedidos para ${nombre} a las ${hora}`)
+}
+
 
 
